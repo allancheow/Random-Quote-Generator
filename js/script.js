05198f21fa13bf603e24196e558e240fb4f89419
Allan Cheow <allan.cheow132@gmail.com>
2021-02-18 10:11:20 -0800

    Refactored code and removed unnecessary comments

--- js/script.js
+++ js/script.js
@@ -6,46 +6,22 @@ project 1 - A Random Quote Generator
   // Quote to test script connection, just for fun... and it's Admiral Ackbar!!!
   console.log(`"It's a trap!" - Admiral Ackbar (Erik Bauersfeld), Star Wars: Return of the Jedi, 1983`);
 
-// For assistance: 
-  // Check the "Project Resources" section of the project instructions
-  // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
-
-/*** 
- * `quotes` array 
-***/
-
-  // Placed quotes array into it's own file. js/quotes.js 
-  // and added new script tag in index.html
-  // This action was in regards to "Build an Object Challenge"
-  // Place quotes.js above script.js for loading order
-  // - Allan C.
-
 /***
  * `getRandomQuote` function
 ***/
 
 function getRandomQuote() {
-  // 1. Create a variable that generates a random number
-  // between zero and the last index in the `quotes` array
 
   /**
    * An arrow expression to randomly generate a number
-   * from 0 through the maxinum number of objects in 
+   * from 0 through the maximum number of objects in 
    * the quotes array.
    * 
    * @param {number} quotes - Used to set upper limit of number generator
    * @returns {number} A random number
-   * @author Allan Cheow
    */
 
   const randNumber = () => Math.floor( Math.random() * quotes.length );
-    // Testing my random number generator
-    //console.log(`My random number: ${randNumber()}`);
-
-  // 2. Use the random number variable and bracket notation 
-  // to grab a random object from the `quotes` array, and 
-  // store it in a variable
-  // 3. Return the variable storing the random quote object
 
   return quotes[randNumber(quotes)];
 }
@@ -55,68 +31,26 @@ function getRandomQuote() {
 ***/
 
 function printQuote() {
-  // 1. Create a variable that calls the getRandomQuote() 
-  // function
-
   let randomQuote = getRandomQuote();
-
-  // 2. Create a variable that initiates your HTML string with 
-  // the first two <p></p> elements, their classNames, 
-  // and the quote and source properties, but leave off 
-  // the second closing `</p>` tag for now
-
-  console.log(`Quote: ${randomQuote.quote}`);
-  console.log(`Source: ${randomQuote.source}`);
   let html = `<p class="quote">${randomQuote.quote}</p><p class="source">${randomQuote.source}`;
 
-  // 3. Use an if statement to check if the citation property 
-  // exists, and if it does, concatenate a <span></span> 
-  // element, appropriate className, and citation property 
-  // to the HTML string
-
-  if ( randomQuote.citation ) {
-    console.log(`Citation: ${randomQuote.citation}`);
-    html += `<span class="citation">${randomQuote.citation}</span>`;
-  }
-
-  // 4. Use an if statement to check of the year property exists, 
-  // and if it does, concatenate a <span></span> element, 
-  // appropriate className, and year property to the HTML 
-  //string
+  randomQuote.citation ? html += `<span class="citation">${randomQuote.citation}</span>` : null;
   
-  if ( randomQuote.year ) {
-    console.log(`Year: ${randomQuote.year}`);
-    html += `<span class="year">${randomQuote.year}</span>`;
-  }
+  randomQuote.year ? html += `<span class="year">${randomQuote.year}</span>` : null;
 
-  if ( randomQuote.tag ) {
-    console.log(`Tag: ${randomQuote.tag}`);
-    html += `<span class="year"><br>${randomQuote.tag}</span>`;
-  }
-
-  // 5. After the two if statements, concatenate the closing </p> 
-  // tag to the HTML string
+  randomQuote.tag ? html += `<span class="year"><br>${randomQuote.tag}</span>` : null;
 
   html += `</p>`;
 
-  // 6. set the innerHTML of the quote-box div to equal the 
-  // complete HTML string
-
-  // Given code by Treehouse to replace content in the
-  // "quote-box" div with content in html variable
-  // - Allan C.
   document.getElementById('quote-box').innerHTML = html;
   
   /*** Exceeds Expectations conditions - 
    * Background color changes to a random color each time the quote refreshes.
   ***/
   /**
-   * An arrow expression to randomly generate a number
-   * from 0 through 256 everytime it's called. 
-   * No @params used in the function.
+   * Randomly generate a number from 0 through 225
    * 
    * @returns {number} A random number
-   * @author Allan Cheow
    */
   const randomNumGen = () => Math.floor(Math.random() * 225);
   /**
@@ -126,37 +60,20 @@ function printQuote() {
    * 
    * @param {number} value - Random number from randomNumGen()
    * @returns {string} A random RGB color value
-   * @author Allan Cheow
    */
   const randomRGB = value => `rgb( ${value()}, ${value()}, ${value()} )`;
-  // Selects the <body> tag and replaces the styling of background-color
-  // Used W3School for reference: https://www.w3schools.com/jsref/dom_obj_body.asp
-  // - Allan C.
   document.querySelector('body').style.backgroundColor = randomRGB(randomNumGen);
 
 };
 
 /**
  * Created an expression function which turns on and off
- * the auto quote generator on button activation.
- * 
- * @author Allan Cheow
- */
+ * the auto quote generator on button activation. */
 
 const autoButtonText = document.querySelector('#auto-quote');
 
 const autoPrintQuote = () => {
-  if ( autoButtonText.textContent === 'Auto Generate' ) {
-    // Based on Treehouse FSJS Project Study Guide
-    // reference: https://www.w3schools.com/jsref/met_win_setinterval.asp
-    autoQuoter = setInterval(printQuote, 5000);
-    autoButtonText.textContent = `End Auto Generate`;
-  } else {
-    autoButtonText.textContent = `Auto Generate`;
-    // Based on Treehouse FSJS Project Study Guide
-    // reference: https://www.w3schools.com/jsref/met_win_clearinterval.asp
-    clearInterval(autoQuoter);
-  }
+  autoButtonText.textContent === 'Auto Generate' ? (autoQuoter = setInterval(printQuote, 5000), autoButtonText.textContent = `End Auto Generate`) : (autoButtonText.textContent = `Auto Generate`, clearInterval(autoQuoter));
 }
 
 /***
